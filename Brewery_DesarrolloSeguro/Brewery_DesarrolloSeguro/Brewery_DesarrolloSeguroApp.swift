626aae4bbc186a6c98589b0d4f65f415b3f6286d
Ana Ganfornina <anies.llamani@gmail.com>
2025-06-20 17:16:33 +0200

    InitialComit

---
+++ Brewery_DesarrolloSeguro/Brewery_DesarrolloSeguro/Brewery_DesarrolloSeguroApp.swift
@@ -0,0 +1,17 @@
+//
+//  Brewery_DesarrolloSeguroApp.swift
+//  Brewery_DesarrolloSeguro
+//
+//  Created by Ana on 20/6/25.
+//
+
+import SwiftUI
+
+@main
+struct Brewery_DesarrolloSeguroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Brewery_DesarrolloSeguro/Brewery_DesarrolloSeguro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Brewery_DesarrolloSeguro
+//
+//  Created by Ana on 20/6/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

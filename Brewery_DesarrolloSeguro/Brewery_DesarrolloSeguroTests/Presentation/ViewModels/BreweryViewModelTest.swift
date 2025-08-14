6bec3761e98b3ffe1b4d0afc533f22881fa84902
Ana Ganfornina <anies.llamani@gmail.com>
2025-08-14 14:00:37 +0200

    test: add BreweryViewModel unit tests for  brewery loading

--- Brewery_DesarrolloSeguro/Brewery_DesarrolloSeguroTests/Presentation/ViewModels/BreweryViewModelTest.swift
+++ Brewery_DesarrolloSeguro/Brewery_DesarrolloSeguroTests/Presentation/ViewModels/BreweryViewModelTest.swift
@@ -7,29 +7,69 @@
 
 import XCTest
 
+@testable import Brewery_DesarrolloSeguro
+
 final class BreweryViewModelTest: XCTestCase {
-
+    var sut: BreweryViewModel!
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        try super.setUpWithError()
+        KeychainHelper.keychain.deleteBrewery()
+        sut = BreweryViewModel(useCase: BreweriesUseCaseMock())
     }
-
+    
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        sut = nil
+        try super.tearDownWithError()
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    
+    func test_getBreweries() async throws {
+        
+        // Given
+        // Usamod una expectation para esperar a que nos informe de los cambios de estado el viewModel
+        
+        var expectedBreweries: [Brewery] = []
+        
+        
+        
+        // When
+        
+        let expectation = expectation(description: "ViewModel load brewery and inform")
+        
+        Task{
+            while sut.beweryData.isEmpty {
+                try? await Task.sleep(nanoseconds: 50_000_000) // 0.05s
+            }
+            expectedBreweries = sut.beweryData
+            expectation.fulfill()
         }
+        
+        
+        
+        // Then
+        
+        wait(for: [expectation], timeout: 0.1)
+        
+        // Como sabemos que el mock devuelve dos breweries
+        XCTAssertEqual(sut.beweryData.count, 2)
+        let brewery = try XCTUnwrap(sut.beweryData.first)
+        let breweryExpected = try XCTUnwrap(expectedBreweries.first)
+        
+        XCTAssertEqual(brewery.id, breweryExpected.id)
+        
+        
+        // Podemos comprobar datos concretos si queremos
+        XCTAssertEqual(sut.beweryData[0].name, "Bière de la Plaine Mock")
+        XCTAssertEqual(sut.beweryData[1].name,  "La Minotte Mock")
+        
     }
-
+    
+    
+    
 }
+
+// Given
+
+// When
+
+// Then
